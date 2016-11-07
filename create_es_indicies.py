38d20f98cb047b9c3258012561a2f4d833d824a7
ian mulvany <ian@mulvany.net>
2016-11-07 15:43:47 +0000

    created wrapping funcs for testing

--- create_es_indicies.py
+++ create_es_indicies.py
@@ -15,50 +15,56 @@ def create_es_index(indexname, mapping):
     if not exist:
         es.indices.create(index=indexname, body=request_body)
 
-request_body = {
-        'mappings': {
-            'doi_queue': {
-                  "_timestamp":{
-                    "enabled": "true"
-                  },
-                'properties': {
-                    'issn': {'index': 'not_analyzed', 'type': 'string'},
-                    'doi': {'index': 'not_analyzed', 'type': 'string'}
-                }}}
-    }
-indexname = doi_queue
-create_es_index(indexname, request_body)
+def create_cursor_index(indexname):
+    request_body = {
+            'mappings': {
+                'issn_cursor': {
+                      "_timestamp":{
+                        "enabled": "true"
+                      },
+                    'properties': {
+                        'issn': {'index': 'not_analyzed', 'type': 'string'},
+                        'cursor': {'index': 'not_analyzed', 'type': 'string'},
+                        'datetime': {'index': 'not_analyzed', 'format': 'dateOptionalTime', 'type': 'date'}
+                    }}}
+        }
+    create_es_index(indexname, request_body)
 
-request_body = {
-        'mappings': {
-            'issn_cursor': {
-                  "_timestamp":{
-                    "enabled": "true"
-                  },
-                'properties': {
-                    'issn': {'index': 'not_analyzed', 'type': 'string'},
-                    'cursor': {'index': 'not_analyzed', 'type': 'string'},
-                    'datetime': {'index': 'not_analyzed', 'format': 'dateOptionalTime', 'type': 'date'}
-                }}}
-    }
-indexname = cursor_index
-create_es_index(indexname, request_body)
+def create_doi_queue_index(indexname):
+    request_body = {
+            'mappings': {
+                'doi_queue': {
+                      "_timestamp":{
+                        "enabled": "true"
+                      },
+                    'properties': {
+                        'issn': {'index': 'not_analyzed', 'type': 'string'},
+                        'doi': {'index': 'not_analyzed', 'type': 'string'}
+                    }}}
+        }
+    create_es_index(indexname, request_body)
 
-request_body = {
-        'mappings': {
-            'crossref_md': {
-                  "_timestamp":{
-                    "enabled": "true"
-                  },
-                'properties': {
-                    'DOI': {'index': 'not_analyzed', 'type': 'string'},
-                    'URL': {'index': 'not_analyzed', 'type': 'string'},
-                    'fulltext': {'index': 'not_analyzed', 'type': 'string'},
-                    'author': {'index': 'not_analyzed', 'type': 'string'},
-                    'title': {'index': 'analyzed', 'type': 'string'},
-                    'ISSN': {'index': 'not_analyzed', 'type': 'string'},
-                    'pub_date': {'index': 'not_analyzed', 'format': 'dateOptionalTime', 'type': 'date'}
-                }}}
-    }
-indexname = crossref_index
-create_es_index(indexname, request_body)
+def create_crossref_index(indexname):
+    request_body = {
+            'mappings': {
+                'crossref_md': {
+                      "_timestamp":{
+                        "enabled": "true"
+                      },
+                    'properties': {
+                        'DOI': {'index': 'not_analyzed', 'type': 'string'},
+                        'URL': {'index': 'not_analyzed', 'type': 'string'},
+                        'fulltext': {'index': 'not_analyzed', 'type': 'string'},
+                        'author': {'index': 'not_analyzed', 'type': 'string'},
+                        'title': {'index': 'analyzed', 'type': 'string'},
+                        'ISSN': {'index': 'not_analyzed', 'type': 'string'},
+                        'pub_date': {'index': 'not_analyzed', 'format': 'dateOptionalTime', 'type': 'date'}
+                    }}}
+        }
+    create_es_index(indexname, request_body)
+
+
+if __name__ == main():
+    create_crossref_index(crossref_index)
+    create_doi_queue_index(doi_queue)
+    create_cursor_index(cursor_index)
